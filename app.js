2923c86ebad66dd119fba1442384600febfb0f9b
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-03-23 15:39:04 +0500

    Set up express-session

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const path = require("node:path");
 const express = require("express");
+const session = require("express-session");
 const app = express();
+const pool = require("./db/pool");
+const pgSession = require("connect-pg-simple")(session);
 
 require("dotenv").config();
 
@@ -11,6 +14,24 @@ app.set("view engine", "ejs");
 
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
+
+const sessionStore = new pgSession({
+  pool: pool,
+  tableName: "user_sessions",
+  createTableIfMissing: true,
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 const PORT = process.env.PORT;
 

e190c1bec9636a5904c775eeb9a85e632c6a94a3
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-03-27 23:16:52 +0500

    add routes for join-the-club route

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,12 +4,30 @@ const {
   signUpPost,
   signUpGet,
   loginGet,
+  joinTheClubGet,
+  joinTheClubPost,
 } = require("../controllers/indexController");
+const passport = require("passport");
 const indexRouter = Router();
+
+indexRouter.get("/", (req, res, next) => {
+  console.log(req.session);
+  res.send("Hey!");
+});
 
 indexRouter.get("/sign-up", signUpGet);
 indexRouter.post("/sign-up", signUpValidation, signUpPost);
+indexRouter.get("/join-the-club", joinTheClubGet);
+indexRouter.get("/join-the-club", joinTheClubPost);
 
 indexRouter.get("/login", loginGet);
+indexRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/",
+  }),
+  (req, res, next) => {}
+);
 
 module.exports = indexRouter;

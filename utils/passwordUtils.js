4e2b50b96c97cd604d3f648fde9e05c8b6e359ac
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-03-27 23:17:16 +0500

    Change the functionality of utils

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -1,11 +1,13 @@
+const bcrypt = require("bcrypt");
+
 async function genPassword(password) {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   return hashedPassword;
 }
 
-async function validPassword(password, userPassword) {
+async function validatePassword(password, userPassword) {
   const match = await bcrypt.compare(password, userPassword);
   return match;
 }
 
-module.exports = { genPassword, validPassword };
+module.exports = { genPassword, validatePassword };

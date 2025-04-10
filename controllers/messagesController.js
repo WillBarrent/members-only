73aabc8cf9c879f9f5ca9cfdb809f88fb5f2c5e5
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-04-11 00:05:07 +0500

    Create new message post request method

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,6 +1,23 @@
-async function createMessageGet(req, res, next) {}
+const { createNewMessage } = require("../db/db");
 
-async function createMessagePost(req, res, next) {}
+async function createMessageGet(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.render("new-message");
+  }
+
+  return res.render("login");
+}
+
+async function createMessagePost(req, res, next) {
+  const date = new Date(Date.now());
+  const added = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+  const { title, message } = req.body;
+  const authorId = req.session.passport.user;
+
+  await createNewMessage(title, message, added, authorId);
+
+  res.redirect("/");
+}
 
 module.exports = {
   createMessageGet,

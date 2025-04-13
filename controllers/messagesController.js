0ec8f02aa75529edfe4225ed528dffcea1bc63c5
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-04-13 15:57:23 +0500

    Always check if there's a username property in session, and share it to the db

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,7 +1,12 @@
-const { createNewMessage } = require("../db/db");
+const { createNewMessage, getUserName } = require("../db/db");
 
 async function createMessageGet(req, res, next) {
   if (req.isAuthenticated()) {
+    if (!req.session.hasOwnProperty("username")) {
+      const username = await getUserName(req.session.passport.user);
+      req.session.username = username;
+    }
+
     return res.render("new-message");
   }
 
@@ -12,9 +17,9 @@ async function createMessagePost(req, res, next) {
   const date = new Date(Date.now());
   const added = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
   const { title, message } = req.body;
-  const authorId = req.session.passport.user;
+  const author = req.session.username;
 
-  await createNewMessage(title, message, added, authorId);
+  await createNewMessage(title, message, added, author);
 
   res.redirect("/");
 }
